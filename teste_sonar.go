87f03c321586567e784f3743639022770b506682
sfcardoso1 <sfcardoso1@gmail.com>
2018-10-23 12:58:06 -0300

    Remove old files

--- teste_sonar.go
+++
@@ -1,8 +0,0 @@
-func (user *User) rename(name string) {
-  name = name  // Noncompliant
-}
-
-func printTen() {
-	myNumber := 010 // Noncompliant. myNumber will hold 8, not 10 - was this really expected?
-	fmt.Println(myNumber)
-}
